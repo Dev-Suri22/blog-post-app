e88991e3d917ef65a9ea96ce4238f29d221020d6
Dev <devsuri220998@gmail.com>
2023-06-08 17:16:14 +0530

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
   root "posts#index"
 end
